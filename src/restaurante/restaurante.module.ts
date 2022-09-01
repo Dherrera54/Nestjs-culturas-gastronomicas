3d66cc2158528e8ffbc6359ce517501385a1a470
Angelica Rodriguez <am.rodriguez12@uniandes.edu.co>
2022-09-01 16:44:30 -0500

    Se crea la logica del recurso restaurante

--- src/restaurante/restaurante.module.ts
+++ src/restaurante/restaurante.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RestauranteEntity } from './restaurante.entity';
 import { RestauranteService } from './restaurante.service';
 
+
 @Module({
+  imports: [TypeOrmModule.forFeature([RestauranteEntity])],
   providers: [RestauranteService]
 })
 export class RestauranteModule {}
