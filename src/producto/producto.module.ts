aa4024fd2e37366dd4c1308258bb594b2d4fd5e4
Wilson Gonzalez <wilson1234567891962@hotmail.com>
2022-09-02 17:11:51 -0500

    Fix

--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CulturaEntity } from '../cultura/cultura.entity';
 import { RecetaEntity } from '../receta/receta.entity';
 import { ProductService } from './product.service';
+import { ProductoEntity } from './producto.entity';
 
 @Module({
   providers: [ProductService],
-  imports: [TypeOrmModule.forFeature([CulturaEntity, RecetaEntity])],
+  imports: [TypeOrmModule.forFeature([ProductoEntity])],
 })
 export class ProductoModule {}
