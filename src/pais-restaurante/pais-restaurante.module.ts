bc566c07169eb0301361cb7a6ff4372da252a74f
ivanbrij <ji.barrerar1@uniandes.edu.co>
2022-09-13 15:25:08 +0000

    Adding controller for Pais ans Pais-Restaurante

--- src/pais-restaurante/pais-restaurante.module.ts
+++ src/pais-restaurante/pais-restaurante.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestauranteEntity } from '../restaurante/restaurante.entity';
 import { PaisEntity } from '../pais/pais.entity';
 import { PaisRestauranteService } from './pais-restaurante.service';
+import { PaisRestauranteController } from './pais-restaurante.controller';
 
 @Module({
   providers: [PaisRestauranteService],
   imports: [TypeOrmModule.forFeature([PaisEntity, RestauranteEntity])],
+  controllers: [PaisRestauranteController],
 })
 export class PaisRestauranteModule {}
