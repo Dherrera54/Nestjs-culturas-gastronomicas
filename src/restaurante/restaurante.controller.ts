8c0f259e5641206a9b6dcf5f2e5ebf8270a4e317
Angelica Rodriguez <am.rodriguez12@uniandes.edu.co>
2022-09-14 23:14:03 -0500

    Se agrega seguridad

--- src/restaurante/restaurante.controller.ts
+++ src/restaurante/restaurante.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
@@ -14,22 +15,26 @@ import { RestauranteService } from './restaurante.service';
 import { RestauranteDto } from './restaurante.dto';
 import { RestauranteEntity } from './restaurante.entity';
 import { plainToInstance } from 'class-transformer';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('restaurantes')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class RestauranteController {
   constructor(private readonly restauranteService: RestauranteService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll() {
     return await this.restauranteService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':restauranteId')
   async findOne(@Param('restauranteId') restauranteId: string) {
     return await this.restauranteService.findOne(restauranteId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() restauranteDto: RestauranteDto) {
     const restaurante: RestauranteEntity = plainToInstance(
@@ -39,6 +44,7 @@ export class RestauranteController {
     return await this.restauranteService.create(restaurante);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':restauranteId')
   async update(
     @Param('restauranteId') restauranteId: string,
@@ -51,6 +57,7 @@ export class RestauranteController {
     return await this.restauranteService.update(restauranteId, restaurante);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':restauranteId')
   @HttpCode(204)
   async delete(@Param('restauranteId') restauranteId: string) {
