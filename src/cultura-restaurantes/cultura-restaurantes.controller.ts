57867f74f2ee6832b16475ce47bec3ca1c017344
Angelica Rodriguez <am.rodriguez12@uniandes.edu.co>
2022-09-14 20:10:21 -0500

    hot fix restaurantes

--- src/cultura-restaurantes/cultura-restaurantes.controller.ts
+++ src/cultura-restaurantes/cultura-restaurantes.controller.ts
@@ -1,14 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  Param,
-  Post,
-  Put,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { RestauranteDto } from 'src/restaurante/restaurante.dto';
 import { RestauranteEntity } from 'src/restaurante/restaurante.entity';
@@ -18,60 +8,33 @@ import { CulturaRestaurantesService } from './cultura-restaurantes.service';
 @Controller('culturas')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class CulturaRestaurantesController {
-  constructor(
-    private readonly culturaRestauranteService: CulturaRestaurantesService,
-  ) {}
+    constructor(private readonly culturaRestauranteService: CulturaRestaurantesService){}
 
-  @Post(':culturaId/restaurantes/:restauranteId')
-  async addRestauranteCultura(
-    @Param('culturaID') culturaId: string,
-    @Param('restauranteId') restauranteId: string,
-  ) {
-    return await this.culturaRestauranteService.addRestauranteCultura(
-      culturaId,
-      restauranteId,
-    );
-  }
+    @Post(':culturaId/restaurantes/:restauranteId')
+    async addRestauranteCultura(@Param('culturaID') culturaId: string, @Param('restauranteId') restauranteId: string){
+       return await this.culturaRestauranteService.addRestauranteCultura(culturaId, restauranteId);
+    }
 
-  @Get(':culturaId/restaurantes/:restauranteId')
-  async findRestauranteByCulturaIdRestauranteId(
-    @Param('culturaId') culturaId: string,
-    @Param('restauranteId') restauranteId: string,
-  ) {
-    return await this.culturaRestauranteService.findRestauranteByCulturaIdRestauranteId(
-      culturaId,
-      restauranteId,
-    );
-  }
+    @Get(':culturaId/restaurantes/:restauranteId')
+    async findRestauranteByCulturaIdRestauranteId(@Param('culturaId') culturaId: string, @Param('restauranteId') restauranteId: string){
+       return await this.culturaRestauranteService.findRestauranteByCulturaIdRestauranteId(culturaId, restauranteId);
+    }
 
-  @Get(':culturaId/restaurantes')
-  async findRestaurantesByCulturaId(@Param('culturaId') culturaId: string) {
-    return await this.culturaRestauranteService.findRestaurantesByCulturaId(
-      culturaId,
-    );
-  }
+    @Get(':culturaId/restaurantes')
+    async findRestaurantesByCulturaId(@Param('culturaId') culturaId: string){
+       return await this.culturaRestauranteService.findRestaurantesByCulturaId(culturaId);
+    }
 
-  @Put(':culturaId/restaurantes')
-  async associateRestaurantesCultura(
-    @Body() restaurantesDto: RestauranteDto[],
-    @Param('culturaId') culturaId: string,
-  ) {
-    const restaurantes = plainToInstance(RestauranteEntity, restaurantesDto);
-    return await this.culturaRestauranteService.associateRestaurantesCultura(
-      culturaId,
-      restaurantes,
-    );
-  }
+    @Put(':culturaId/restaurantes')
+   async associateRestaurantesCultura(@Body() restaurantesDto: RestauranteDto[], @Param('culturaId') culturaId: string){
+       const restaurantes = plainToInstance(RestauranteEntity, restaurantesDto)
+       return await this.culturaRestauranteService.associateRestaurantesCultura(culturaId, restaurantes);
+   }
 
-  @Delete(':culturaId/restaurantes/:restauranteId')
-  @HttpCode(204)
-  async deleteRestauranteCultura(
-    @Param('culturaId') culturaId: string,
-    @Param('restauranteId') restauranteId: string,
-  ) {
-    return await this.culturaRestauranteService.deleteRestauranteCultura(
-      culturaId,
-      restauranteId,
-    );
-  }
+    @Delete(':culturaId/restaurantes/:restauranteId')
+    @HttpCode(204)
+    async deleteRestauranteCultura(@Param('culturaId') culturaId: string, @Param('restauranteId') restauranteId: string){
+       return await this.culturaRestauranteService.deleteRestauranteCultura(culturaId, restauranteId);
+    }
+
 }
