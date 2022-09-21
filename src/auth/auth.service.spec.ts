551177ce7f21c7a20ea128fce11e2f0acd3c80ad
Dario_herrera <d.herrerag@uniandes.edu.co>
2022-09-20 19:45:04 -0500

    Se agregan providers a auth service.spec para arreglar prueba unitaria

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, UserService, JwtService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
