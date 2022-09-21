d3ac804b0d4803facb8d0108454458a9e75451de
Dario_herrera <d.herrerag@uniandes.edu.co>
2022-09-20 21:39:24 -0500

    se implementa cache en cultura

--- src/cultura/cultura.module.ts
+++ src/cultura/cultura.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CulturaService } from './cultura.service';
 import { CulturaEntity } from './cultura.entity';
 import { CulturaController } from './cultura.controller';
 import { JwtService } from '@nestjs/jwt';
+import * as sqliteStore from 'cache-manager-sqlite';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CulturaEntity])],
+  imports: [TypeOrmModule.forFeature([CulturaEntity]), CacheModule.register()],
   providers: [CulturaService, JwtService],
   controllers: [CulturaController],
 })
