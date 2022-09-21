830bf005570089da115607491f8886fec8c0a86a
Angelica Rodriguez <am.rodriguez12@uniandes.edu.co>
2022-09-20 22:37:45 -0500

    Se corrige error de usuario con permisos espcificos

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -16,7 +16,7 @@ export class UserService {
     new User(2, 'user', 'admin', [Action.Read]), //usuario con permisos de lectura para todos los recursos
     new User(3, 'user_2', 'admin', [Action.Create, Action.Update]), //usuario con permisos de escritura (creación y actualización)
     new User(4, 'user_3', 'admin', [Action.Delete]), //con permisos de eliminación
-    new User(5, 'user', 'test', [Action.READ_PRODUCT]),
+    new User(5, 'user_4', 'test', [Action.READ_PRODUCT]), //usuario con permisos especificos para el recurso product
   ];
 
   async findOne(username: string): Promise<User | undefined> {
